9f95ce43dacc3c5baaf3859a5454c24d479b4dc9
atoool <athulchelad@gmail.com>
2021-03-11 22:03:33 +0530

    moment js added and time zone added

--- src/utils/formatAmPm.js
+++ src/utils/formatAmPm.js
@@ -1,13 +1,6 @@
-function formatAmPm(date) {
-  let dt = new Date(date);
-  var minutes = dt.getMinutes();
-  var hours = dt.getHours();
-  var AmPm = hours >= 12 ? 'PM' : 'AM';
-  hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? '0' + minutes : minutes;
-  var strTime = hours + ':' + minutes + ' ' + AmPm;
+import moment from 'moment-timezone';
+
+export default function formatAmPm(date) {
+  var strTime = moment(date).tz('Asia/Dubai').format('HH:MM A');
   return strTime;
 }
-
-export default formatAmPm;
