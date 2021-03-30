49fd0e5777c2b1e1c5c9745b39f2369fd7ff70a5
atoool <athulchelad@gmail.com>
2021-03-30 18:26:33 +0530

    pack scan module UI fix

--- src/screens/pack/ScanScreen.js
+++ src/screens/pack/ScanScreen.js
@@ -3,9 +3,9 @@ import {
   Linking,
   SafeAreaView,
   ScrollView,
+  StyleSheet,
   Text,
   ToastAndroid,
-  TouchableOpacity,
   View,
 } from 'react-native';
 import { RNCamera } from 'react-native-camera';
@@ -16,6 +16,8 @@ import { PackerContext } from '../../context/PackerContext';
 import Button from '../../components/Button';
 import ModalComponent from '../../components/ModalComponent';
 import Loader from '../../components/Loader';
+import TouchLink from '../../components/TouchLink';
+import BarcodeMask from '../../components/BarcodeMask';
 
 const ScanScreen = ({
   navigation,
@@ -104,52 +106,22 @@ const ScanScreen = ({
   };
 
   return (
-    <SafeAreaView style={{ backgroundColor: Colors.WHITE, flex: 1 }}>
+    <SafeAreaView style={styles.container}>
       {loading ? (
         <Loader fullScreen />
       ) : (
         <ScrollView
           showsVerticalScrollIndicator={false}
-          contentContainerStyle={{
-            margin: 30,
-            flex: 1,
-            justifyContent: 'space-between',
-            alignItems: 'center',
-          }}>
+          contentContainerStyle={styles.contentContainer}>
           <View>
             {orderId === '#' && (
-              <Text
-                style={{
-                  textAlign: 'center',
-                  marginBottom: 20,
-                  ...Typography.bold20,
-                }}>
-                {locale?.SS_scanbar}
-              </Text>
+              <Text style={styles.scanBar}>{locale?.SS_scanbar}</Text>
             )}
-            <Text
-              style={{
-                textAlign: 'center',
-                // marginTop: 10,
-                borderWidth: 0.5,
-                color: Colors.lightGray,
-                borderColor: Colors.lightGray,
-                ...Typography.normal12,
-                padding: 10,
-              }}>
-              {locale?.SS_scanbarText}
-            </Text>
+            <Text style={styles.scanBarText}>{locale?.SS_scanbarText}</Text>
           </View>
-          <View
-            style={{
-              height: 'auto',
-              width: '80%',
-              alignItems: 'center',
-              justifyContent: 'center',
-              marginVertical: 20,
-            }}>
+          <View style={styles.cameraBox}>
             <RNCamera
-              style={{ height: '35%', width: '80%' }}
+              style={styles.camera}
               type={RNCamera.Constants.Type.back}
               flashMode={RNCamera.Constants.FlashMode.on}
               androidCameraPermissionOptions={{
@@ -159,12 +131,7 @@ const ScanScreen = ({
                 buttonNegative: locale?.cancel,
               }}
               notAuthorizedView={
-                <View
-                  style={{
-                    height: '35%',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                  }}>
+                <View style={styles.notPermittedBox}>
                   <Button
                     title={locale?.SS_enableCam}
                     onPress={() => Linking.openSettings()}
@@ -174,46 +141,24 @@ const ScanScreen = ({
               captureAudio={false}
               onBarCodeRead={onScan}
             />
-            <BarCodeMask />
+            <BarcodeMask />
           </View>
-          <Text
-            style={{
-              textAlign: 'center',
-              marginTop: 10,
-              color: Colors.lightGray,
-              ...Typography.normal12,
-            }}>
-            {locale?.SS_scanningCode} ...
-          </Text>
+          <Text style={styles.scanningText}>{locale?.SS_scanningCode} ...</Text>
           {totalItem && (
             <>
-              <View
-                style={{
-                  height: 40,
-                  width: 200,
-                  alignSelf: 'center',
-                  justifyContent: 'center',
-                }}>
+              <View style={styles.progressBox}>
                 <Progress.Bar
                   progress={itemScanned / totalItem}
                   height={30}
                   width={200}
-                  color="#c9d1ff"
+                  color={Colors.progress}
                 />
-                <Text
-                  style={{
-                    position: 'absolute',
-                    height: '99%',
-                    width: '100%',
-                    textAlignVertical: 'center',
-                    textAlign: 'center',
-                    fontWeight: 'bold',
-                  }}>
+                <Text style={styles.itemScannedText}>
                   {locale?.SS_scanning} {itemScanned} of {totalItem}
                 </Text>
               </View>
               <View>
-                <LinkButton
+                <TouchLink
                   title={locale?.SS_scanmis}
                   onPress={() => setMismatchModal(true)}
                   onLongPress={
@@ -241,103 +186,62 @@ const ScanScreen = ({
   );
 };
 
-const BarCodeMask = () => {
-  return (
-    <>
-      <View
-        style={{
-          position: 'absolute',
-          top: 20,
-          left: 40,
-          backgroundColor: '#566ae6',
-          height: 3,
-          width: 40,
-        }}
-      />
-      <View
-        style={{
-          position: 'absolute',
-          top: 20,
-          left: 40,
-          backgroundColor: '#566ae6',
-          height: 40,
-          width: 3,
-        }}
-      />
-      <View
-        style={{
-          position: 'absolute',
-          bottom: 20,
-          left: 40,
-          backgroundColor: '#566ae6',
-          height: 3,
-          width: 40,
-        }}
-      />
-      <View
-        style={{
-          position: 'absolute',
-          bottom: 20,
-          left: 40,
-          backgroundColor: '#566ae6',
-          height: 40,
-          width: 3,
-        }}
-      />
-      <View
-        style={{
-          position: 'absolute',
-          top: 20,
-          right: 40,
-          backgroundColor: '#566ae6',
-          height: 3,
-          width: 40,
-        }}
-      />
-      <View
-        style={{
-          position: 'absolute',
-          top: 20,
-          right: 40,
-          backgroundColor: '#566ae6',
-          height: 40,
-          width: 3,
-        }}
-      />
-      <View
-        style={{
-          position: 'absolute',
-          bottom: 20,
-          right: 40,
-          backgroundColor: '#566ae6',
-          height: 3,
-          width: 40,
-        }}
-      />
-      <View
-        style={{
-          position: 'absolute',
-          bottom: 20,
-          right: 40,
-          backgroundColor: '#566ae6',
-          height: 40,
-          width: 3,
-        }}
-      />
-    </>
-  );
-};
-
-const LinkButton = ({ title, onPress, style, onLongPress }) => (
-  <TouchableOpacity
-    style={[{ backgroundColor: 'rgba(0,0,0,0)' }, style]}
-    onPress={onPress}
-    onLongPress={onLongPress}
-    hitSlop={{ top: 20, bottom: 20, left: 20, right: 20 }}>
-    <Text style={{ color: Colors.secondaryRed, ...Typography.normal12 }}>
-      {title}
-    </Text>
-  </TouchableOpacity>
-);
+const styles = StyleSheet.create({
+  container: { backgroundColor: Colors.WHITE, flex: 1 },
+  contentContainer: {
+    margin: 30,
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  scanBarText: {
+    textAlign: 'center',
+    // marginTop: 10,
+    borderWidth: 0.5,
+    color: Colors.lightGray,
+    borderColor: Colors.lightGray,
+    ...Typography.normal12,
+    padding: 10,
+  },
+  cameraBox: {
+    height: '35%',
+    width: '100%',
+    marginVertical: 20,
+  },
+  camera: {
+    flex: 1,
+    overflow: 'hidden',
+  },
+  notPermittedBox: {
+    height: '35%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  scanningText: {
+    textAlign: 'center',
+    marginTop: 10,
+    color: Colors.lightGray,
+    ...Typography.normal12,
+  },
+  progressBox: {
+    height: 40,
+    width: 200,
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  itemScannedText: {
+    position: 'absolute',
+    height: '99%',
+    width: '100%',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  scanBar: {
+    textAlign: 'center',
+    marginBottom: 20,
+    ...Typography.bold20,
+  },
+});
 
 export default ScanScreen;
