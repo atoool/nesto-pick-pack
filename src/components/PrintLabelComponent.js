49ffff3bd76f6e102616e6fa88519eb666e465bf
Basil Arackal <basilarackal@outlook.com>
2021-09-22 13:44:27 +0530

    Print Order ID

--- src/components/PrintLabelComponent.js
+++ src/components/PrintLabelComponent.js
@@ -1,34 +1,68 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Share from 'react-native-share';
 import Barcode from 'react-native-barcode-builder';
-// import ViewShot from 'react-native-view-shot';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import ViewShot from 'react-native-view-shot';
 import { Colors, Typography } from '../styles';
 import Loader from './Loader';
 
-const PrintLabelComponent = ({ orderId, printLabelText, viewShot }) => {
+const PrintLabelComponent = ({ orderId, printLabelText }) => {
+  useEffect(() => {
+    //cons
+  }, []);
+  const printRef = useRef(null);
+  const onPrintAction = () => {
+    printRef.current.capture().then((uri) => {
+      console.log('Print Image URL:', uri);
+      sharePrint(uri);
+    });
+  };
+
+  const sharePrint = async (url) => {
+    const options = {
+      message: orderId,
+      url,
+      title: `Print: ${orderId}`,
+      type: 'image/jpg',
+    };
+    Share.open(options)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        err && console.log(err);
+      });
+  };
+
   return (
     <>
       <View style={styles.labelContainer}>
         <View style={styles.labelTextView}>
           {orderId ? (
-            // <ViewShot
-            //   ref={(re) => {
-            //     if (viewShot) {
-            //       viewShot.current = re;
-            //     }
-            //   }}
-            //   options={{ format: 'jpg', quality: 0.9 }}>
-            <>
-              <Text style={styles.orderIdStyle}>#{orderId}</Text>
-              <Barcode value={orderId} height={50} width={1} />
-            </>
+            <ViewShot
+              ref={(re) => {
+                if (printRef) {
+                  printRef.current = re;
+                }
+              }}
+              options={{ format: 'jpg', quality: 0.9 }}>
+              <>
+                <Text style={styles.orderIdStyle}>#{orderId}</Text>
+                <Barcode value={orderId} height={50} width={1} />
+              </>
+            </ViewShot>
           ) : (
-            // </ViewShot>
             <Loader small />
           )}
         </View>
         <Text style={styles.printLabelText}>{printLabelText}</Text>
       </View>
+      <TouchableOpacity onPress={onPrintAction}>
+        <View style={styles.buttonContainer}>
+          <Text style={styles.printText}>Print</Text>
+        </View>
+      </TouchableOpacity>
     </>
   );
 };
@@ -40,6 +74,20 @@ const styles = StyleSheet.create({
     borderRadius: 12,
     alignItems: 'center',
     padding: 20,
+  },
+  buttonContainer: {
+    backgroundColor: Colors.secondaryRed,
+    borderRadius: 12,
+    alignItems: 'center',
+    padding: 20,
+    marginTop: 10,
+    justifyContent: 'center',
+  },
+  printText: {
+    flex: 1,
+    textAlign: 'center',
+    textAlignVertical: 'bottom',
+    ...Typography.bold16White,
   },
   barcode: { flex: 1 },
   printLabelText: {
